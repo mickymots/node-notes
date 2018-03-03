b55f71550a230baddcfc71aa07e2459eafc1ccd6
Amit Prakash Singh <amit.prakash.singh@accenture.com>
2018-03-03 19:52:21 +0000

    Added lodash module and its uses

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const util = require('./util');
+const _ = require('lodash');
 
 console.log('\n----- app started ------\n');
 
 const res = util.add(1,1);
 
-console.log(res);
+const resultIntIsString = _.isString(1);
+const resultBooleanIsString = _.isString(true);
+const resultStringIsString = _.isString("true")
 
-console.log('\n----- app ended ------\n')
+const resultArray= [resultIntIsString, resultBooleanIsString, resultStringIsString];
+
+
+
+resultArray.forEach(util.arrayPrinter);
+
+const filteredArr = _.uniq(resultArray);
+filteredArr.forEach(util.arrayPrinter)
+
+
+console.log(res);
+console.log('\n----- app ended ------\n');
--- util.js
+++ util.js
@@ -1,8 +1,17 @@
 console.log('\n ---- util.js loaded ------\n');
 
+/** Utility function to print array elements with index */
+const arrayPrinter = (element, index) => {
+  console.log(`result [${index}]= ${element}`)
+}
+
+
 module.exports.add = (x, y) =>{
     console.log('x = ' + x);
     console.log(`y = ${y}`);
 
     return x+y;
 }
+
+
+module.exports.arrayPrinter = arrayPrinter;
